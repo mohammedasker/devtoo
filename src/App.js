9fe78c3d1cb26bd1e7f21d6aa31d58b7ba2ef4b0
Mohammed Asker <zetoanime@gmail.com>
2020-04-15 10:57:01 +0300

    created a navbar and added its contents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+import Navbar from "./components/navbar/navbar";
+
+import "./App.css";
+
 function App() {
 	return (
-		<div>
-			<header>top header</header>
-			<h1>another header</h1>
+		<div className="container">
+			<Navbar />
+			<h1>header</h1>
 			<main>some posts</main>
 		</div>
 	);
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./navbar.css";
+
+const Navbar = () => (
+	<div className="topnav">
+		<a href="/">
+			<i className="fab fa-dev fa-3x logo"></i>
+		</a>
+		<input type="text" placeholder="Search..." className='nav-search'/>
+		<button className="new-post">Write a post</button>
+		<i className="far fa-comment nav-icons"></i>
+		<i className="far fa-bell nav-icons"></i>
+		<i className="fas fa-bars nav-icons"></i>
+	</div>
+);
+
+export default Navbar;
