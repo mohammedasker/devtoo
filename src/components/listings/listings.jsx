e2b22413180872f902f60b92eea7abfafdced4b8
Mohammed Asker <zetoanime@gmail.com>
2020-04-21 17:34:39 +0300

    created a listings component and added its contents

---
+++ src/components/listings/listings.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+import "./listings.css";
+
+const Listings = () => (
+	<div className="listings-container">
+		<div className="listings-top">
+			<p className="listings-header">
+				<b>Listings</b>
+			</p>
+			<p className="listings-number">78</p>
+			<button className="listings-button">-</button>
+		</div>
+		<ul>
+			<li className="listings-list"></li>
+			<li className="listings-list">
+				<span role="img" aria-label="rocket">
+					🚀
+				</span>
+				[LIMITED PROMO]
+				<span role="img" aria-label="rocket">
+					🚀
+				</span>
+				90% OFF all programming courses lifetime plan - ONLY FIRST 100 ACCOUNTS!{" "}
+				<span role="img" aria-label="party">
+					🎉🎉
+				</span>{" "}
+				education <br />
+				<span className="small-text">education</span>
+			</li>
+			<li className="listings-list">
+				MESG: launch advanced applications quicker & faster cfp
+				<br />
+				<span className="small-text">cfp</span>
+			</li>
+			<li className="listings-list">
+				Looking for tech content writers{" "}
+				<span role="img" aria-label="money">
+					💰
+				</span>{" "}
+				jobs
+				<br />
+				<span className="small-text">jobs</span>
+			</li>
+			<li className="listings-list">
+				OVERPASS: Personalized Pair Programming Sessions! mentors
+				<br />
+				<span className="small-text">mentors</span>
+			</li>
+			<li className="listings-list">
+				CTOs Talk K8s and SaaS
+				<br />
+				<span className="small-text">events</span>
+			</li>
+			<li className="listings-text">CREATING A LISTINGS</li>
+		</ul>
+		<hr />
+	</div>
+);
+
+export default Listings;
--- src/components/sidebars/sidebar-right.jsx
+++ src/components/sidebars/sidebar-right.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import "./sidebar-right.css";
 import SignUp from "../sign-up/sign-up";
 import EventsCard from "../events-card/events-card";
+import Listings from "../listings/listings";
 
 const SidebarRight = () => (
 	<div className="sidebar-right-container">
 		<SignUp />
 		<EventsCard />
+		<Listings />
 	</div>
 );
 
