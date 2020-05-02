7aa6172ec8c05cd7208d1c1d05b20365f0484b05
Mohammed Asker <zetoanime@gmail.com>
2020-05-02 15:04:39 +0300

    added hovering menu to the navbar and made it mobile responsive,but not perfect yet

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,7 +11,22 @@ const Navbar = () => (
 		<button className="new-post">Write a post</button>
 		<i className="far fa-comment nav-icons"></i>
 		<i className="far fa-bell nav-icons"></i>
-		<i className="fas fa-bars nav-icons"></i>
+		<div className="dropdown">
+			<i className="fas fa-bars nav-icons"></i>
+			<div className="dropdown-content">
+				<ul className="dropdown-content-lists">
+					<li className="nav-menu-list">
+						<b>Sign in/Up</b>
+					</li>
+					<div className="nav-menu-nested-list">
+						<li className="nav-menu-list">Via Twitter</li>
+						<li className="nav-menu-list">Via GitHub</li>
+					</div>
+					<hr />
+					<li className="nav-menu-list">All about DEV</li>
+				</ul>
+			</div>
+		</div>
 	</div>
 );
 
