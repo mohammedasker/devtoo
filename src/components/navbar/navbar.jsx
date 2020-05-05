50df32cab627006bbd75653c86cde55c7d6129a9
Mohammed Asker <zetoanime@gmail.com>
2020-05-05 13:23:50 +0300

    added hovering text to icons and display follow buttons on hover in my tags component

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,14 +5,14 @@ import "./navbar.css";
 const Navbar = () => (
 	<div className="topnav">
 		<a href="/">
-			<i className="fab fa-dev fa-3x logo"></i>
+			<i title="App logo" className="fab fa-dev fa-3x logo"></i>
 		</a>
 		<input type="text" placeholder="Search..." className="nav-search" />
 		<button className="new-post">Write a post</button>
-		<i className="far fa-comment nav-icons"></i>
-		<i className="far fa-bell nav-icons"></i>
+		<i title="Connect" className="far fa-comment nav-icons"></i>
+		<i title="Notifications" className="far fa-bell nav-icons"></i>
 		<div className="dropdown">
-			<i className="fas fa-bars nav-icons"></i>
+			<i title="Navigation menu" className="fas fa-bars nav-icons"></i>
 			<div className="dropdown-content">
 				<ul className="dropdown-content-lists">
 					<li className="nav-menu-list">
--- src/components/social-media/social-media.jsx
+++ src/components/social-media/social-media.jsx
@@ -4,11 +4,11 @@ import "./social-media.css";
 
 const SocialMedia = () => (
 	<div className="social-icons">
-		<i className="fab fa-twitter social-icon"></i>
-		<i className="fab fa-facebook-square social-icon"></i>
-		<i className="fab fa-github social-icon"></i>
-		<i className="fab fa-instagram social-icon"></i>
-		<i className="fab fa-twitch social-icon"></i>
+		<i title="Twitter" className="fab fa-twitter social-icon"></i>
+		<i title="Facebook" className="fab fa-facebook-square social-icon"></i>
+		<i title="GitHub" className="fab fa-github social-icon"></i>
+		<i title="Instagram" className="fab fa-instagram social-icon"></i>
+		<i title="Twitch" className="fab fa-twitch social-icon"></i>
 	</div>
 );
 
