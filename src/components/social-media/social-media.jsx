097dda90b6d6917674051a969fb5fb6957e86d10
Mohammed Asker <zetoanime@gmail.com>
2020-05-18 15:41:47 +0300

    fixed social media links linking to DEV pages, show follow tag buttons on mobile screen, and added DEV logos to asset folder

--- src/components/social-media/social-media.jsx
+++ src/components/social-media/social-media.jsx
@@ -4,19 +4,39 @@ import "./social-media.css";
 
 const SocialMedia = () => (
 	<div className="social-icons">
-		<a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+		<a
+			href="https://twitter.com/thepracticaldev"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
 			<i title="Twitter" className="fab fa-twitter social-icon"></i>
 		</a>
-		<a href="https://facebook.com/" target="_blank" rel="noopener noreferrer">
+		<a
+			href="https://www.facebook.com/thepracticaldev"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
 			<i title="Facebook" className="fab fa-facebook-square social-icon"></i>
 		</a>
-		<a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+		<a
+			href="https://github.com/thepracticaldev"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
 			<i title="GitHub" className="fab fa-github social-icon"></i>
 		</a>
-		<a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
+		<a
+			href="https://www.instagram.com/thepracticaldev/"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
 			<i title="Instagram" className="fab fa-instagram social-icon"></i>
 		</a>
-		<a href="https://twitch.tv/" target="_blank" rel="noopener noreferrer">
+		<a
+			href="https://www.twitch.tv/thepracticaldev"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
 			<i title="Twitch" className="fab fa-twitch social-icon"></i>
 		</a>
 	</div>
