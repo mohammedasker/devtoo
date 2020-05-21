d9a616556fd5504573549e51e445eebefd6897be
Mohammed Asker <zetoanime@gmail.com>
2020-05-21 15:24:32 +0300

    hide bolt and database icons for desktop screens and added a link to event component

--- src/components/events-card/events-card.jsx
+++ src/components/events-card/events-card.jsx
@@ -6,12 +6,16 @@ import codeland from "../../assets/codeland.png";
 
 const EventsCard = () => (
 	<div className="event-container">
-		<img
-			src={codeland}
-			alt="codeland distributed event"
-			className="event-image"
-		/>
-		<h2 className="event-card-text">Register Today</h2>
+		<a href="https://codelandconf.com/">
+			<img
+				src={codeland}
+				alt="codeland distributed event"
+				className="event-image"
+			/>
+		</a>
+		<a href="https://codelandconf.com/" className="event-link">
+			<h2 className="event-card-text">Register Today</h2>
+		</a>
 	</div>
 );
 
