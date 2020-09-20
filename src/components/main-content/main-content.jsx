9cac604af950ae3ac85ec882571cc9033d33415b
Mohammed Asker <zetoanime@gmail.com>
2020-09-20 12:37:24 +0300

    added head article component and made dev card component responsive

--- src/components/main-content/main-content.jsx
+++ src/components/main-content/main-content.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import SingleArticle from "../single-article/single-article";
 import NavArticle from "../nav-article/nav-article";
 import DevCard from "../dev-card/dev-card";
+import HeadArticle from "../head-article/head-article";
 
 import "./main-content.css";
 
 const MainContent = () => (
 	<div className="main-content-container">
 		<NavArticle />
+		<HeadArticle />
 		<SingleArticle />
 		<SingleArticle />
 		<SingleArticle />
